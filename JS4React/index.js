4e2bbecb39a8b51def635c50d32869c1fb4ad652
121038217 <125855991+121038217@users.noreply.github.com>
2024-05-20 15:41:05 -0600

    creación de archivos html, css y js

---
+++ JS4React/index.js
@@ -0,0 +1,20 @@
+// funcion
+/*
+function suma(a,b){
+    return a+b;
+}
+
+console.log(suma(2,3));
+*/
+
+
+/*funcion fatArrow
+const suma= (a,b)=>{
+    return a+b
+}
+*/
+
+//fat Arrow Function simplificada
+const suma= (a,b)=> a+b 
+
+console.log(suma(2,3))
