78b29a101a8599eb0d243c05299eba34e73d1b05
121038217 <125855991+121038217@users.noreply.github.com>
2024-05-24 15:34:15 -0600

    Finalización de la practica 3: array, modulos: export & import

--- JS4React/index.js
+++ JS4React/index.js
@@ -43,7 +43,7 @@ console.log(usuario.enviaCorreo())
 
 // TRABAJO CON EL DOM
 
-const titulo= document.createElement('h1')  //Creamos una constante llamada titulo
+/*const titulo= document.createElement('h1')  //Creamos una constante llamada titulo
 titulo.innerText='Soy un titulo creado con JS'  //Agregamos un texto al titulo
 document.body.append(titulo)
 
@@ -54,7 +54,77 @@ document.body.append(boton)
 
 // para que funcione el botón
 boton.addEventListener('click',function(){
-    // console.log('Evento click ejecutado')
+    // console.log('Evento click ejecutado') 
     titulo.innerText="Evento click ejecutado" //modificamos el titulo
     alert('Exito')
 })
+*/
+
+
+// ****************** TRABAJO CON ARREGLOS ******************
+
+const nombres= ['Alan','Isa','Pablo','Juan Luis']
+
+/*for(let i= 0 ; i< nombres.length; i++){
+    const elemento=nombres[i]
+    console.log(elemento)
+}
+
+nombres.forEach(function (nombre){
+    console.log(nombre)
+}) 
+*/
+
+// -------------------------------- ARRAY 2 -----------------------------
+/*
+const array2= nombres.map(function(nombre){
+    console.log(nombre)
+    return nombre
+})
+
+console.log(nombres)
+console.log(array2)
+*/
+
+// ------------------- FUNCION FIND: Ubicar elemetos en un arreglo --------------------
+/*
+const resultado= nombres.find(function(nombre){
+    if(nombre === 'Pablo'){
+        return nombre
+    }
+})
+
+console.log(resultado)
+*/
+
+// -------------- FUNCION Filter: Recorre el arreglo filtrando un resultado en otro -----
+/*
+const resultado= nombres.filter(function(nombre){
+    if(nombre != 'Pablo'){
+        return nombre
+    }
+})
+
+console.log(nombres)
+console.log(resultado)
+*/
+
+// -------------- FUNCION Concat: -----
+/*
+const edades= [78,89,12,8,10,15]
+console.log(nombres.concat(edades))
+
+// Spread Operator
+console.log([...nombres,...edades])
+*/
+
+// ********** MODULOS: IMPORT & EXPORT *********** 
+
+import { suma,resta} from "./calculadora.js"
+
+console.log(suma(4,3))
+console.log(resta(10,3))
+
+
+
+
