b772ddc8658940491b0005797650b1be45174413
121038217 <125855991+121038217@users.noreply.github.com>
2024-06-26 15:01:19 -0600

    sectionList

--- HolaMundo/App.js
+++ HolaMundo/App.js
@@ -1,32 +1,44 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View, SectionList } from 'react-native';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [submit, setSubmit] = useState('');
-
   return (
     <View style={styles.container}>
-
-      <ScrollView style={styles.scrollView}>
-      
-      <Text> Componente TextInput: {submit}</Text>  
-      <Text> Componente TextInput: {submit}</Text>  
-      <Text> Componente TextInput: {submit}</Text>  
-      <Text> Componente TextInput: {submit}</Text>  
-      <Text> Componente TextInput: {submit}</Text>  
-      
-      <TextInput  style={styles.input}  placeholder="Escribe un texto" onChangeText={t => setText(t)} defaultValue={text}/> {/* onchangeText  mandar llamar el setText, defaultValue mandar llamar el text de arriba*/}
-      
-      <TouchableOpacity style={styles.button} onPress={() => { setSubmit(text); alert('Texto enviado'); }}>
-        
-      <Text style={styles.buttonText}>Presioname...</Text>
-      
-      </TouchableOpacity>
-
-      </ScrollView>
-
+      <SectionList
+        sections={[
+          { title: 'Grupo A',
+            data: [
+              { key: '1', name: 'Lilian' },
+              { key: '2', name: 'Monse' },
+              { key: '3', name: 'Alan' },
+              { key: '4', name: 'Elias' },
+            ]
+          },
+          { title: 'Grupo B',
+            data: [
+              { key: '5', name: 'Pablo' },
+              { key: '6', name: 'Victor' },
+              { key: '7', name: 'El profe' },
+            ]
+          },
+          { title: 'Grupo C',
+            data: [
+              { key: '8', name: 'Fer' },
+              { key: '9', name: 'Juan' },
+              { key: '10', name: 'Dani' },
+              { key: '11', name: 'Diego' },
+            ]
+          },
+        ]}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text style={styles.section}>{section.title}</Text>
+        )}
+        keyExtractor={(item) => item.key}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -35,36 +47,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column', /* se usa para dar orientación al texto, ya se a que es row o colum */
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center', /* felx-start posición al inicio/ flex-end posición al final/strech toma todo el espacio */
-    justifyContent: 'center', /* Alinea de arriba hacia bajo, flex-end alinea hacia bajo, space-around alinea en una posición equitativa, en el space-evvenly alinia los espacios */
+    alignItems: 'stretch', // 'stretch' está escrito incorrectamente en tu código
+    justifyContent: 'center',
+    paddingTop: 50,
   },
-  input: {
-    backgroundColor: '276B92',
-    width: '50%',
-    height: 40,
-    borderBottomColor: '#000000',
+  item: {
+    padding: 10,
+    fontSize: 24,
+    height: 50,
+    borderColor: 'blue',
     borderBottomWidth: 1,
   },
-  button: {
-    backgroundColor: "#FF69B4",  // Color de fondo rosa del botón
-    paddingVertical: 15,         // Espaciado vertical dentro del botón
-    paddingHorizontal: 25,       // Espaciado horizontal dentro del botón
-    borderRadius: 30,            // Bordes redondeados
-    alignItems: 'center',        // Alineación del contenido del botón
-    borderWidth: 5,              // Ancho del borde
-    borderColor: '#d6d7da',      // Color del borde
-    marginTop: 20,
-  },
-  buttonText: {
-    color: '#fff',               // Color del texto
-    fontSize: 16,                // Tamaño del texto
-    fontWeight: 'bold',          // Peso del texto
-    letterSpacing: 1,            // Espaciado entre letras
-    textTransform: 'uppercase',  // Transformación del texto a mayúsculas
-  },
-  scrollView: {
-    width:Dimensions.get('window').width
+  section: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: '#eee',
+    paddingTop: 4,
+    paddingBottom: 4,
+    paddingLeft: 10, // Agregué un padding izquierdo para mejorar la apariencia
   },
 });
